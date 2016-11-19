89ba1590cb7201d6356228c471963dd76f25cded
Masanori Nomura <nom_sk8@hotmail.com>
2016-11-19 12:24:14 +0900

    HelloWorld.javaのdaoへの置き換え完了

--- Nasca/src/pisces/HelloWorld.java
+++ Nasca/src/pisces/HelloWorld.java
@@ -11,13 +11,8 @@ import java.io.PrintWriter;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
- 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Servlet implementation class HelloWorld
@@ -36,71 +31,48 @@ public class HelloWorld extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ElementDAO dao = new ElementDAO();
-		dao.SelectByID("PROCEDURE1");
-		dao.SelectAll();
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 		PrintWriter out = response.getWriter();
-		Connection con = null;
-	    PreparedStatement pstmt = null;
-	    ResultSet rs = null;
-	    try {
-			// データソースの取得
-			Context ctx = new InitialContext();
-			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/test");
-			 
-			// データベースへ接続
-			con = ds.getConnection();
-			 
-			// SQLの実行
-			pstmt = con.prepareStatement("select elmtid,elmtnm,elmtTP from M_element");
-			rs = pstmt.executeQuery();
+		
+		ElementDAO dao = new ElementDAO();
+		List<Element> elements = dao.SelectAll();
+		Iterator<Element> itr = elements.iterator();
+		
+		// JsonFactoryの生成
+		JsonFactory jsonFactory = new JsonFactory();
+		// JsonGeneratorの取得
+		JsonGenerator generator = jsonFactory.createGenerator(out);
 			
-			// JsonFactoryの生成
-			JsonFactory jsonFactory = new JsonFactory();
-			// JsonGeneratorの取得
-			JsonGenerator generator = jsonFactory.createGenerator(out);
+		//オブジェクトの書き込み
+		generator.writeStartArray();
+		while(itr.hasNext()){
+			Element element = itr.next();
+			String parent = "";
+			String[] idStrings = element.getId().split("\\.");
 			
-			//オブジェクトの書き込み
-			generator.writeStartArray();
-			while(rs.next()){				
-				String parent = "";
-				String[] idStrings = rs.getString("elmtid").split("\\.");
-				
-				if(idStrings.length == 1){
-					parent = "#";
-				} else{
-					//区切り文字"."で完全IDを区切り末尾IDを除いたIDを親ノードとします。
-					for(int i = 0; i < idStrings.length - 1; i++){
-						parent = parent + "." + idStrings[i];
-					}
-					parent = parent.substring(1, parent.length());
+			//親ノードの設定を行います
+			if(idStrings.length == 1){
+				parent = "#";
+			} else{
+				//区切り文字"."で完全IDを区切り末尾IDを除いたIDを親ノードとします。
+				for(int i = 0; i < idStrings.length - 1; i++){
+					parent = parent + "." + idStrings[i];
 				}
-				
-				generator.writeStartObject();
-				generator.writeStringField("id",rs.getString("elmtID"));
-				generator.writeStringField("parent", parent);
-				generator.writeStringField("text", rs.getString("elmtNM"));
-				generator.writeStringField("type", rs.getString("elmttP"));
-				generator.writeEndObject();
-				
+				parent = parent.substring(1, parent.length());
 			}
-			generator.writeEndArray();
+			
+			generator.writeStartObject();
+			generator.writeStringField("id",element.getId());
+			generator.writeStringField("parent", parent);
+			generator.writeStringField("text", element.getName());
+			generator.writeStringField("type", element.getType());
+			generator.writeEndObject();
+			
+		}
+		generator.writeEndArray();
 
-			//ストリームへの書き出し
-			generator.flush();
-	    } catch (Exception e) {
-	      System.out.println(e.getMessage());
-	      throw new ServletException(e);
-	    } finally {
-	      try {
-	        rs.close();
-	        pstmt.close();
-	        con.close();
-	      } catch (Exception e) {
-	      }
-	    }
+		//ストリームへの書き出し
+		generator.flush();
 	}
 
 	/**
